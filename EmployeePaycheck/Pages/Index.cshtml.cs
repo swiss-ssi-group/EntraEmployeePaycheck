91288b94faca616233ab77a11188e996a06b03b2
damienbod <damien_bod@hotmail.com>
2023-07-06 22:34:24 +0200

    redirect

--- EmployeePaycheck/Pages/Index.cshtml.cs
+++ EmployeePaycheck/Pages/Index.cshtml.cs
@@ -1,18 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace EmployeePaycheck.Pages;
 
 public class IndexModel : PageModel
 {
-    private readonly ILogger<IndexModel> _logger;
-
-    public IndexModel(ILogger<IndexModel> logger)
+    public IActionResult OnGet()
     {
-        _logger = logger;
-    }
-
-    public void OnGet()
-    {
-
+        return Redirect("/Paycheck/PaycheckEnterIdS1");
     }
 }
